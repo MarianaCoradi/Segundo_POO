102a1c64019c0d4805057f42f6ac4edd21f4820f
Mariana Coradi <mariana4bribas@gmail.com>
2021-12-08 14:33:31 -0300

    Criando uma classe Mãe e fazendo com que as classes filhas herdem seus métodos, porém os atributos estão ainda duplicados

--- modelo.py
+++ modelo.py
@@ -1,57 +1,45 @@
 
-class Filme:
-    def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
+
+class ProgramaDeTv:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
         self.ano = ano
-        self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
 
     def dar_like(self):
-        self.__likes += 1
+        self._likes += 1
 
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
 
     @nome.setter
     def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
+        self._nome = novo_nome.title()
 
     @property
     def likes(self):
-        return self.__likes
+        return self._likes
 
     @likes.setter
     def likes(self, likes):
-        self.__likes = likes
+        self._likes = likes
 
 
-class Serie:
+class Filme(ProgramaDeTv):
+    def __init__(self, nome, ano, duracao):
+        self._nome = nome.title()
+        self.ano = ano
+        self.duracao = duracao 
+        self._likes = 0
+
+
+class Serie(ProgramaDeTv):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.__likes = 0
-
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    @likes.setter
-    def likes(self, likes):
-        self.__likes = likes
-
+        self._likes = 0
 
 
 vingadores = Filme("vigandores", 2018, 160)

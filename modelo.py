6915c14fc1f748a057f02f51b5265a8f4f23563f
Mariana Coradi <mariana4bribas@gmail.com>
2021-12-08 11:33:14 -0300

    Adicionando métodos dar like e adicionando property e setter nos atibutos nome e likes

--- modelo.py
+++ modelo.py
@@ -1,20 +1,64 @@
 
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.__likes = 0
+
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    @likes.setter
+    def likes(self, likes):
+        self.__likes = likes
 
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.__likes = 0
+
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    @likes.setter
+    def likes(self, likes):
+        self.__likes = likes
+
 
 
 vingadores = Filme("vigandores", 2018, 160)
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Temporadas: {vingadores.duracao} minutos')
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Temporadas: {vingadores.duracao} minutos - Likes: {vingadores.likes}')
 
-atlanta = Serie("Atlanta", 2018, 2)
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas}')
+atlanta = Serie('atlanta', 2018, 2)
+atlanta.nome = 'atlanta - de glover'
+atlanta.dar_like()
+atlanta.dar_like()
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')

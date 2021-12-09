177b58e4485a92eaea4755c40784b17fe01f1996
Mariana Coradi <mariana4bribas@gmail.com>
2021-12-09 09:52:08 -0300

    Aprendendo a imprimir objetos de classes filhas com atributos distintos em tempo de execução utilizando a função hasattr e if em uma linha

--- modelo.py
+++ modelo.py
@@ -44,8 +44,13 @@ class Serie(ProgramaDeTv):
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
-print(f'{vingadores.nome} - {vingadores.duracao}: {vingadores.likes}')
+
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_like()
-atlanta.dar_like()
-print(f'{atlanta.nome} - {atlanta.temporadas}: {atlanta.likes}')
+
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    detalhes = programa.duracao if hasattr(programa, "duracao") else programa.temporadas
+    print(f'{programa.nome} - {detalhes}: {programa.likes}')

8b2df10e961804fc14f8f345598fa4e5f9053835
Mariana Coradi <mariana4bribas@gmail.com>
2021-12-09 10:57:10 -0300

    Criando a classe Playlist, porém para imprimir a minha playlist eu teria q saber o atributo programas, pois playlist_fim_de_semana não é iterable

--- modelo.py
+++ modelo.py
@@ -51,15 +51,34 @@ class Serie(ProgramaDeTv):
     def __str__(self):
         return f'Nome: {self.nome} - Ano: {self.ano} - Temporadas: {self.temporadas} : {self._likes} Likes'
 
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.programas = programas
+
+    def tamanho(self):
+        return len(self.programas)
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
-
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
+vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+atlanta.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist('Playlist de fim de semana', filmes_e_series)
 
-for programa in filmes_e_series:
+for programa in playlist_fim_de_semana.programas:
     print(programa)  # o print chamará a função interna do programa que tem para exibir seu conteúdocomo texto
     #não preciso chamar o método explicitamente, como por exemplo, programa.imprime()

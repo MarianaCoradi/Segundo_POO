028aa1af5bea455d56aabb6e5eb9a2e0bec9ae74
Mariana Coradi <mariana4bribas@gmail.com>
2021-12-12 17:34:34 -0300

    Criando Mixins, que são classes onde não queremos criar objetos delas, apenas que outras classes herdem seu comportamento para podermos reutilizar métodos

--- heranca_multipla.py
+++ heranca_multipla.py
@@ -1,5 +1,8 @@
 
 class Funcionario:
+    def __init__(self, nome):
+        self.nome = nome
+
     def registra_horas(self, horas):
         print(f'Horas registradas...{horas} horas')
 
@@ -20,21 +23,29 @@ class Alura(Funcionario):
     def busca_perguntas_sem_resposta(self):
         print('Mostrando perguntas não respondidas do fórum')
 
+class Hipster: #Exemplo de MIXINS
+    def __str__(self):
+        return f'Hipster, {self.nome}' # Não tem o inicializador de nome, pois eu quero q a classe q for herdar hister ja possua esse  atributo
+
 class Junior(Alura): #Funcionário Junior só tem acesso aos métodos da Alura
     pass
 
-class Pleno(Alura, Caelum): #Funcionário Pleno tem acesso tanto as ações da Alura quanto da Caelum --> Exemplo de herança múltipla
+class Pleno(Alura, Caelum): #Funcionário Pleno tem acesso tanto as ações da Alura quanto da Caelum
     pass
 
-jose = Junior()
+class Senior(Alura, Caelum, Hipster): #Funcionário Senior tem acesso tanto as ações da Alura quanto da Caelum e de Hipster(esse último que retorna uma string do nome do funionário para o print)
+    pass
+
+
+jose = Junior('José')
 jose.busca_perguntas_sem_resposta()
 
-luan = Pleno()
+luan = Pleno('Luan')
 luan.busca_perguntas_sem_resposta()
 luan.busca_cursos_do_mes()
 luan.registra_horas(50)
+luan.mostrar_tarefas()
 
-luan.mostrar_tarefas() #Nessa linha de código o luan que teria acesso ao mostrar_tarefas de Alura e Caelum , apenas printa o do Alura!!!!
-# Ordem em que o algoritmo do Python procura os métodos (MRO): Pleno > Alura > Funcionario > Caelum > Funcionario
-#Ou seja, primeiro ele procura na classe filha(Pleno), depois na primeira classe mãe(Alura) e depois na classe mãe da primeira classe mãe(Funcionario)...
-#... apenas depois que ele irá procurar na segunda classe mãe da classe filha (Caelum) e por fim, na classe mãe da segunda classe mãe (no nosso caso, também é Funcionario)
+
+pedrin = Senior('Pedro')
+print(pedrin)
